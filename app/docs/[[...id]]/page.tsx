892a0e07f790a885dccdbc003eb946933b0d5df0
Bennie Robinson <brobinson88@gmail.com>
2024-09-29 15:12:18 -0400

    routing

---
+++ app/docs/[[...id]]/page.tsx
@@ -0,0 +1,20 @@
+type DocsParams = {
+  params: {
+    id: string[]
+  }
+  searchParams: {
+    [key: string]: string | string[] | undefined
+  }
+}
+
+const DocsPage = ({ params, searchParams }: DocsParams) => {
+  return (
+    <div>
+      Docs: {params.id.join(', ')}
+      <br />
+      <div>{searchParams.thing}</div>
+    </div>
+  )
+}
+
+export default DocsPage
